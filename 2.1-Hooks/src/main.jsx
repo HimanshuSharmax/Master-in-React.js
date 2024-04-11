6dd1c400ece633380547e42518d5b34f0f16f67d
HimanshuSharmax <ihimanshusharmax@gmail.com>
2024-04-11 15:42:30 +0530

    All Hooks- useCallback useMemo useReducer useRef

--- 2.1-Hooks/src/main.jsx
+++ 2.1-Hooks/src/main.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { AppProvider } from './components/useContext/UseContext'
+// import MemoHook from './components/useMemo/MemoHook'
+// import LayoutEffect from './components/useLayoutEffect/LayoutEffect'
+// import RefHook1 from './components/useRef/RefHook1'
+// import RefHook from './components/useRef/RefHook'
+// import { AppProvider } from './components/useContext/UseContext'
+// import ReducerHook from './components/useReducer/ReducerHook'
 // import ClearUp from './components/useEffect/ClearUp'
 // import UseState from './components/useState/useState.jsx'
 // import UseStateObject from './components/useState/UseStateObject.jsx'
 // import UseEffect from './components/useEffect/UseEffect'
+import Callbackhook from './components/useCallback/Callbackhook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   {/* <UseState/> */}
-  //   {/* <UseStateObject/> */}
-  //   {/* <UseEffect/> */}
-  //   {/* <ClearUp/> */}
-  //   {/* <UseContext/> */}
-  // </React.StrictMode>,
-  <AppProvider>
-      
-  </AppProvider>
+  <React.StrictMode>
+    {/* <UseState/> */}
+    {/* <UseStateObject/> */}
+    {/* <UseEffect/> */}
+    {/* <ClearUp/> */}
+    {/* <UseContext/> */}
+    {/* <ReducerHook/> */}
+    {/* <RefHook1/> */}
+    {/* <RefHook/> */}
+    {/* <LayoutEffect/> */}
+    {/* <MemoHook/> */}
+    <Callbackhook/>
+  </React.StrictMode>,
 )
